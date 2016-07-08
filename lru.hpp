248d37de91b53c6f5ab4d3c9b1dc25e995c4e097
proller <proller@github.com>
2016-07-09 02:48:52 +0300

    works as need

--- lru.hpp
+++ lru.hpp
@@ -1,9 +1,14 @@
-#include <cstddef>
-#include <string>
+/*
+
+lru cache with defined maximum memory usage
+
+*/
+
+#include <string> //only for default type
 #include <list>
 #include <map>
 
-#if defined(LRU_DEBUG)
+#if LRU_DEBUG
 #include <iostream>
 #define LRU_PRINT(a) std::cout << a;
 #else
@@ -25,17 +30,21 @@ class lru {
 public:
 
   lru(size_t limit_) :
-    map_node_size(sizeof(void*) * 3),
+    map_node_size(sizeof(void*) * 4), //bit larger than need (~3.5)
     list_node_size(sizeof(void*) * 3),
     limit(limit_) {
     size += sizeof(*this);
-
+    max_size = limit - size;
     LRU_PRINT("initial size=" << size << "\n");
   }
 
 
   bool insert(Key_type & key, Value_type &value) {
-
+    size_t want_size = calc_size_pair(key, value);
+    if (want_size > max_size) {
+      LRU_PRINT("NOT inserted max_size=" << max_size << " want_size=" << want_size << " size=" << size << " storage=" << storage.size() << " sorted_keys=" << sorted_keys.size() << "\n");
+      return false;
+    }
     auto hash = calc_hash(key);
     auto it = storage.find(hash);
     size_t current_size = 0;
@@ -43,9 +52,8 @@ public:
       current_size = calc_stored_size(it);
     }
 
-    size_t want_size = calc_size_pair(key, value);
-
     //need clean cache?
+    size_t erased = 0;
     if (current_size < want_size && (size - current_size) + want_size > limit) {
       //clean
       for (auto sit = sorted_keys.rbegin(); sit != sorted_keys.rend(); ++sit)  {
@@ -56,33 +64,48 @@ public:
         size -= calc_stored_size( storage_it );
         storage.erase( storage_it );
         sorted_keys.erase(storage_it->second.second);
-        LRU_PRINT("cleaning " << calc_stored_size( storage_it ) << " want_size=" << want_size << " size=" << size << " storage=" << storage.size() << " sorted_keys=" << sorted_keys.size() << "\n");
-
+        LRU_PRINT("cleaning hash=" << *sit << " clean" << calc_stored_size( storage_it ) << " want_size=" << want_size << " size=" << size << " storage=" << storage.size() << " sorted_keys=" << sorted_keys.size() << "\n");
+        ++erased;
         if ((size - current_size) + want_size < limit) {
           break;
         }
       }
+
+      if ((size - current_size) + want_size > limit) {
+        // should never happen
+        return false;
+      }
+
     }
 
+    // replace if exists
     if (current_size) {
-      it = storage.find(hash);
+      // get new valid iterator
+      if (erased)
+        it = storage.find(hash);
       sorted_keys.erase(it->second.second);
       sorted_keys.emplace_front(hash);
       //storage[hash] = std::make_pair(value, sorted_keys.begin());
-      //del!!
+      //todo: try replace existing
       storage.erase( it );
       auto pr = storage.emplace(hash, std::make_pair(value, sorted_keys.begin()));
+      it = pr.first;
       size -= current_size;
     } else {
       sorted_keys.emplace_front(hash);
       auto pr = storage.emplace(hash, std::make_pair(value, sorted_keys.begin()));
-      it = pr.first; //?
+      it = pr.first;
     }
     //size += calc_size(it->first) + calc_size(it->second) + map_node_size;
     auto new_size = calc_stored_size(it);
     size += new_size;
 
-    LRU_PRINT("inserted new_size=" << new_size << " want_size=" << want_size << " size=" << size << " storage=" << storage.size() << " sorted_keys=" << sorted_keys.size() << "\n");
+    LRU_PRINT("inserted new_size=" << new_size << " want_size=" << want_size
+              /*<< " key.size="<<key.size()
+              << " key.capacity="<<key.capacity()
+              << " value.size="<< value.size()
+              << " value.capacity="<< value.capacity() */
+              << " size=" << size << " storage=" << storage.size() << " sorted_keys=" << sorted_keys.size() << " hash=" << hash << "\n");
 
     return true;
   }
@@ -97,6 +120,9 @@ public:
 
       sorted_keys.erase(it->second.second);
       sorted_keys.emplace_front(hash);
+      it->second.second = sorted_keys.begin();
+
+      LRU_PRINT("get hash=" << hash << "\n");
 
       *to = &it->second.first;
       return true;
@@ -106,41 +132,56 @@ public:
 
 
   bool remove (Key_type & key) {
-
     auto hash = calc_hash(key);
     auto it = storage.find(hash);
     if (it != storage.end()) {
+      size -= calc_stored_size( it );
       sorted_keys.erase(it->second.second);
       storage.erase(hash);
-      size -= calc_stored_size( it );
       LRU_PRINT("erasing " << " size=" << size << " storage=" << storage.size() << " sorted_keys=" << sorted_keys.size() << "\n");
       return true;
     }
+    LRU_PRINT("NOT erasing " << " size=" << size << " storage=" << storage.size() << " sorted_keys=" << sorted_keys.size() << "\n");
     return false;
   }
 
 private:
   using Key_store_type = size_t;
   using list_iter = std::list<Key_store_type>::iterator;
-  using storage_iter = typename std::map<Key_store_type, std::pair<Value_type, list_iter> >::iterator;
+  using storage_type = std::map<Key_store_type, std::pair<Value_type, list_iter> >;
+  using storage_iter = typename storage_type::iterator;
 
-  std::map<Key_store_type, std::pair<Value_type, list_iter> > storage;
+  storage_type storage;
   std::list<Key_store_type> sorted_keys;
 
   const size_t limit;
   const size_t map_node_size;
   const size_t list_node_size;
   size_t size = 0;
+  size_t max_size = 0;
 
+  // map(hash + val + iterator ) + list(hash)
   inline size_t calc_size_pair(const Key_type & key, const Value_type &value) const {
-    return sizeof(Key_store_type) + calc_size<>(value) + map_node_size + list_node_size;
+    return 0
+           + sizeof(Key_store_type)
+           + calc_size<>(value)
+           + sizeof(list_iter)
+           + map_node_size
+           + sizeof(Key_store_type)
+           + list_node_size;
   };
 
   inline size_t calc_stored_size(const storage_iter & it) const {
-    return sizeof(it->first) + sizeof(it->second.first) + calc_size<>(it->second.second) + map_node_size + list_node_size + sizeof(Key_store_type);
+    return 0
+           + sizeof(it->first)  // map key (hash)
+           + calc_size<>(it->second.first)  // map value
+           + sizeof(it->second.second) // map iterator to list
+           + map_node_size // map internal
+           + sizeof(Key_store_type) // list
+           + list_node_size // list internal
+           ;
   };
 
-  //inline Key_store_type calc_hash(const Key_type &key) const {
   inline Key_store_type calc_hash(Key_type &key) const {
     hash_func fn;
     return fn(key);
--- test.cpp
+++ test.cpp
@@ -1,16 +1,34 @@
 /*
 
 astyle --style=attach -s2 --keep-one-line-blocks --keep-one-line-statements --pad-oper lru.hpp test.cpp
-g++ -std=c++14 -lstdc++ test.cpp
-
+g++ -std=c++11 -lstdc++ test.cpp
+g++ -std=c++11 -lstdc++ -DLRU_DEBUG=1 test.cpp
 */
 
-#define LRU_DEBUG 1
+//#define LRU_DEBUG 1
+
+#include <iostream>
+//#include <thread>
+
 #include "lru.hpp"
 
 int main () {
 
-  lru<> l1(10000);
+
+//std::this_thread::sleep_for(std::chrono::seconds(100));
+// mem here: 13312  1572
+
+#if !LRU_DEBUG
+  // real mem from top: 997504 987208
+  lru<> lb1(1000000000);
+  std::cout << "little datas\n";
+  for(int i = 0; i <= 100000000; ++i) {
+   std::string keyb(std::to_string(i));
+   std::string valb(1, 'b');
+   lb1.insert(keyb, valb);
+  }
+#endif
+
 
 
   lru<int, int> l_int(20000);
@@ -23,17 +41,72 @@ int main () {
   for(int i = 10; i <= 20000; ++i) {
     int i3 = i * 3;
     l_int.insert(i, i3);
+    int ig = 11;
+    if (l_int.get(ig, &to))
+      std::cout << "get " << ig << " = " << *to << "\n";
+    ig = 15;
+    if (l_int.get(ig, &to))
+      std::cout << "get " << ig << " = " << *to << "\n";
+  }
+
+  // 20 must be cleared because not used
+  int ign = 20;
+  if (l_int.get(ign, &to)) {
+    std::cout << "WRONG! get " << ign << " = " << *to << "\n";
+  } else {
+    std::cout << "OK get " << ign << " = " << to << "\n";
   }
 
   lru<double> l_double(30000);
 
+  lru<> l1(10000);
   std::string key1("a");
-  std::string val1("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+  std::string val1(1000, 'b');
   l1.insert(key1, val1);
 
 
-  key1="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-  val1="b";
+  key1 = std::string(100, 'a');
+  val1 = "b"; // capacity still ~1000
   l1.insert(key1, val1);
+  val1 = "bbbbbbbbbbb";
+  val1.shrink_to_fit();
+  l1.insert(key1, val1);
+  val1 = std::string(500, 'c');
+
+  l1.insert(key1, val1);
+  l1.remove(key1);
+  l1.remove(key1);
+  l1.remove(key1);
+
+  // bigger than storage, dont even try store
+  val1 = std::string(500000, 'c');
+  l1.insert(key1, val1);
+
+  //exactly as free space
+  lru<> l2(10000);
+  val1 = std::string(10000 - sizeof(l2) - 104, 'c');
+  l2.insert(key1, val1);
+  val1 = std::string(1, 'c');
+  val1.shrink_to_fit();
+  l2.insert(key1, val1);
+
+#if !LRU_DEBUG
+
+  // real mem from top: 990044 979604
+  lru<> lb(1000000000);
+  std::cout << "big datas\n";
+  for(int i = 0; i <= 100000; ++i) {
+   std::string keyb(std::to_string(i));
+   std::string valb(1000000, 'b');
+   lb.insert(keyb, valb);
+  }
+  std::cout << "little datas\n";
+  for(int i = 0; i <= 100000000; ++i) {
+   std::string keyb(std::to_string(i));
+   std::string valb(1, 'b');
+   lb.insert(keyb, valb);
+  }
+#endif
+
 
 }
